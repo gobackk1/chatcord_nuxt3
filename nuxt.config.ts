cb0f274cea7dadc0fd0a50fb12abe948f4d8813f
gobackk1 <k.kwbt4@icloud.com>
2023-02-09 08:50:19 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  builder: 'webpack',
+  typescript: {
+    shim: false,
+  },
+  webpack: {},
+  imports: {},
+  app: {},
+})
