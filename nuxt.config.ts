f6185ef89bf0f6034d03285b06132f97f1a741f4
gobackk1 <k.kwbt4@icloud.com>
2023-02-09 22:41:01 +0900

    install vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,27 @@
+import vuetify from 'vite-plugin-vuetify'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
   typescript: {
     shim: false,
   },
-  webpack: {},
-  imports: {},
-  app: {},
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify())
+      })
+    },
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
 })
