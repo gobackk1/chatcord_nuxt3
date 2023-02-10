29d768d941dd47f9c79eddfc080f4f7d2a6cb11a
gobackk1 <k.kwbt4@icloud.com>
2023-02-10 10:58:16 +0900

    install prettier and eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,31 +1,23 @@
 module.exports = {
-  root: true,
   env: {
     es6: true,
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/typescript',
+    'google',
+    '../.eslintrc.js',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'functions/tsconfig.dev.json'],
+    sourceType: 'module',
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
+    '/lib/**/*', // Ignore built files.
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
+    'import/no-unresolved': 0,
   },
-};
+}
